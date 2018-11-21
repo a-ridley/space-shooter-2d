e733b571a55d993e0d85fe2282af777a29c38fea
David T <djtai@protonmail.com>
2018-11-20 23:28:55 -0800

    Update GameManager.cs
    GameManager functions include GameOver(), BossDefeated(), and RestartLevel()

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameOver = false;
+	bool bossDefeated = false;
+
+	public float restartDelay = 5f;
+
+	public GameObject completeLevelUI;
+
+	public void GameOver() {
+		// Player died before defeating the boss
+		Debug.Log ("Game over");
+		Invoke ("RestartLevel", restartDelay);
+	}
+
+	public void BossDefeated() {
+		// Boss is defeated -> start next level
+		Debug.Log ("Boss defeated");
+		completeLevelUI.SetActive (true);
+	}
+
+	void RestartLevel() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	void NextLevel() {
+
+	}
+}
